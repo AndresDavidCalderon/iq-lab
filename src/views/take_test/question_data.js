fa11755b55459dbc3322bfa31af21f805d706819
thepromaper <david.voidgames@gmail.com>
2023-05-24 21:55:53 -0500

    rotation question

---
+++ src/views/take_test/question_data.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    question: 'which one is the odd one',
+    image: 'rotation/rotation_test.svg',
+    answers: [
+      'rotation/rotation_answer_left',
+      'rotation/rotation_answer_right',
+      'rotation/rotation_answer_up',
+      'rotation/rotation_answer_down',
+    ],
+    correct_index: 3,
+  },
+];
