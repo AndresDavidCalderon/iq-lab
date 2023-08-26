16dabe60bda9426bff6275539f77ac1189a2255b
Andres David Calderon <david.voidgames@gmail.com>
2023-08-26 13:20:32 -0500

    allow manual runs

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'no-console': 0,
     'import/no-extraneous-dependencies': ['error', { devDependencies: true, optionalDependencies: false, peerDependencies: false }],
+    'linebreak-style': 0,
   },
 };
