ceab55db92c170347056f3c225c684480def466f
Andres David Calderon <david.voidgames@gmail.com>
2023-08-26 14:05:53 -0500

    try with wildcard

--- tests/question_integrity.test.js
+++ tests/question_integrity.test.js
@@ -4,7 +4,7 @@ import { pathToFileURL } from 'url';
 import { SVG, registerWindow } from '@svgdotjs/svg.js';
 import { createSVGWindow } from 'svgdom';
 
-const questions = (await import(pathToFileURL(path.resolve(__dirname, '../src/views/take_test/questions/question_data.js')))).default;
+const questions = (await import(pathToFileURL(__dirname, '../src/views/take_test/questions/question_data.js'))).default;
 
 const window = createSVGWindow();
 const { document } = window;
