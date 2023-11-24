11bee44572a98bb9f1be56cfa1d8b251d847f9f4
Andres David Calderon <david.voidgames@gmail.com>
2023-11-23 20:47:14 -0500

    make integrity test allow text questions

--- src/views/take_test/Questions/question_test.js
+++ src/views/take_test/Questions/question_test.js
@@ -1,2 +1,13 @@
 export default [
+  {
+    questionType: 'text',
+    question: '',
+    statement: 'Enunciado de prueba, esta parte debe proveer la info necesaria para resolver la pregunta.',
+    answers: [
+      'respuesta A',
+      'respuesta B',
+      'respuesta C',
+      'respuesta C, la correcta.',
+    ],
+  },
 ];
