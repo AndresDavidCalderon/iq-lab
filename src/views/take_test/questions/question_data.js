f0b47600152cba981bcad12c5290c39e73f48277
Andres David Calderon <david.voidgames@gmail.com>
2023-12-04 14:20:39 -0500

    question data completed

--- src/views/take_test/questions/question_data.js
+++ src/views/take_test/questions/question_data.js
@@ -536,6 +536,20 @@ const questions = [
     answerShape: 'multiple_files',
     format: 'svg',
   },
+  {
+    question: 'Move from one circle to one next to it, starting on the bottom left, and ending on the top right',
+    name: 'number_path',
+    difficulty: 3,
+    answers: [
+      '36',
+      '16',
+      '29',
+      '32',
+    ],
+    explanation: 'On the path right,up 3 times, right 3 times and up, we get 2+4+4+4+4+4+4+2+4, which is equals to 32.',
+    answerShape: 'multiple_files',
+    format: 'svg',
+  },
 ];
 console.log(questions.length);
 export default testQuestions.length > 0 ? testQuestions : questions;
