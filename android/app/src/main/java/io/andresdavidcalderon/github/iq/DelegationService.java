7fb57285503f6920e80a752e7bf7b3f58db7241e
Andres David Calderon <david.voidgames@gmail.com>
2023-12-23 16:09:11 -0500

    android port with bubblewrap

---
+++ android/app/src/main/java/io/andresdavidcalderon/github/iq/DelegationService.java
@@ -0,0 +1,14 @@
+package io.andresdavidcalderon.github.iq;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
---
+++ android/app/src/main/java/io/github/andresdavidcalderon/iq/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.andresdavidcalderon.iq;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
